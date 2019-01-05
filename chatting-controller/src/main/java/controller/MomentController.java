4d1514a687804f388f457ff9deaaf6049f5a8066
cpp <cpp@example.com>
2019-01-05 15:53:46 +0800

    create project

--- chatting-controller/src/main/java/controller/MomentController.java
+++ chatting-controller/src/main/java/controller/MomentController.java
@@ -2,8 +2,14 @@ package controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
-@RequestMapping("/moment")
+@RequestMapping(value = "/moment")
 public class MomentController {
+
+    @RequestMapping(value = "/hello",method = RequestMethod.GET)
+    public String hello(){
+        return "login";
+    }
 }
