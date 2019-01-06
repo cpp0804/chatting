f45db63da2d223981f7228a094d1ac21dfa86c0b
cpp <cpp@example.com>
2019-01-06 16:20:35 +0800

    create project

--- chatting-controller/src/main/java/config/ChattingConfiguration.java
+++ chatting-controller/src/main/java/config/ChattingConfiguration.java
@@ -1,37 +1,41 @@
+//
+// Source code recreated from a .class file by IntelliJ IDEA
+// (powered by Fernflower decompiler)
+//
+
 package config;
 
 import org.neo4j.ogm.config.ClasspathConfigurationSource;
 import org.neo4j.ogm.config.ConfigurationSource;
+import org.neo4j.ogm.config.Configuration.Builder;
 import org.neo4j.ogm.session.SessionFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.io.ContextResource;
-import org.springframework.data.neo4j.repository.Neo4jRepository;
 import org.springframework.data.neo4j.repository.config.EnableNeo4jRepositories;
 import org.springframework.data.neo4j.transaction.Neo4jTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-
 
 @Configuration
-@EnableNeo4jRepositories(basePackages = "repository")
+@EnableNeo4jRepositories(basePackages = {"repository"})
 @EnableTransactionManagement
 public class ChattingConfiguration {
+    public ChattingConfiguration() {
+    }
 
     @Bean
     public SessionFactory sessionFactory() {
-        return new SessionFactory(configuration(),"entity","relationship");
+        return new SessionFactory(this.configuration(), new String[]{"entity", "relationship"});
     }
 
     @Bean
     public org.neo4j.ogm.config.Configuration configuration() {
         ConfigurationSource properties = new ClasspathConfigurationSource("ogm.properties");
-        org.neo4j.ogm.config.Configuration configuration = new org.neo4j.ogm.config.Configuration.Builder(properties).build();
+        org.neo4j.ogm.config.Configuration configuration = (new Builder(properties)).build();
         return configuration;
     }
 
     @Bean
     public Neo4jTransactionManager transactionManager() {
-        return new Neo4jTransactionManager(sessionFactory());
+        return new Neo4jTransactionManager(this.sessionFactory());
     }
 }
